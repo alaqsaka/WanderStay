7f0115d3d7d1a422b4b019d03cae22be1a8d4494
alaqsaka <aqsaabidin15@gmail.com>
2023-05-21 16:41:05 +0700

    register modal UI done

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -186,6 +186,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
               {/* End Footer */}
             </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -76,6 +77,44 @@ const RegisterModal = () => {
     </div>
   );
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+        text-neutral-500
+        text-center
+        mt-4
+        font-light
+      "
+      >
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="
+            text-neutral-800 cursor-pointer hover:underline
+          "
+          >
+            Login
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -85,6 +124,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
