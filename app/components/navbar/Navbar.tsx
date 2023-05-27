301f1c303fc656346afd6d3e4b5eb405b0b639c4
alaqsaka <aqsaabidin15@gmail.com>
2023-05-27 17:29:04 +0700

    categories ui

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -31,6 +32,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
