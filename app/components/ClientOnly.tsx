fc7d7dece26d027d5d65ab67bc864f94d8d4d342
alaqsaka <aqsaabidin15@gmail.com>
2023-06-11 20:33:55 +0700

    fix props interface

--- app/components/ClientOnly.tsx
+++ app/components/ClientOnly.tsx
@@ -5,7 +5,7 @@ interface ClientOnlyProps {
   children: React.ReactNode;
 }
 
-const ClientOnly = ({ children }) => {
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
   const [hasMounted, setHasMounted] = useState(false);
 
   useEffect(() => {
